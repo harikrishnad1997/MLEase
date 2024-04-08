98990ca56a496e1ca076e808de0e852bc0304e81
Harikrishna Dev <37830351+harikrishnad1997@users.noreply.github.com>
2024-04-08 17:46:18 -0500

    Updating bucketiser with inplace operations and column specifier

--- mlease/bucketizer.py
+++ mlease/bucketizer.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_is_fitted
+from sklearn.cluster import KMeans
 
 class Bucketizer(BaseEstimator, TransformerMixin):
-    def __init__(self, num_classes=None, labels=None, use_qcut=True, custom_labels=None, automatic_num_classes=False):
+    def __init__(self, num_classes=None, labels=None, use_qcut=True, custom_labels=None, automatic_num_classes=False, columns=None, inplace=False):
         """
         Initialize the Bucketizer.
 
@@ -14,6 +15,8 @@ class Bucketizer(BaseEstimator, TransformerMixin):
         - use_qcut (bool): Whether to use pd.qcut or pd.cut for binning.
         - custom_labels (list): Custom labels for the bins.
         - automatic_num_classes (bool): Whether to automatically determine the number of classes based on data size.
+        - columns (list): Columns to be bucketed.
+        - inplace (bool): Whether to create a new column for the bucketed data or replace the original column.
 
         """
         self.num_classes = num_classes
@@ -21,6 +24,8 @@ class Bucketizer(BaseEstimator, TransformerMixin):
         self.use_qcut = use_qcut
         self.custom_labels = custom_labels
         self.automatic_num_classes = automatic_num_classes
+        self.columns = columns
+        self.inplace = inplace
         self.bins = None
 
     def fit(self, X, y=None):
@@ -43,9 +48,20 @@ class Bucketizer(BaseEstimator, TransformerMixin):
             self.labels = self.custom_labels
 
         self.bins = {}
-        for col in X.columns:
+        for col in self.columns:
             if self.use_qcut:
                 self.bins[col] = pd.qcut(X[col], self.num_classes, labels=self.labels)
+            elif self.binning_method == 'equal_width':
+                for col in X.select_dtypes(include=['int', 'float']).columns:
+                    min_val = X[col].min()
+                    max_val = X[col].max()
+                    bin_edges = np.linspace(min_val, max_val, self.num_classes + 1)
+                    self.bins[col] = pd.cut(X[col], bins=bin_edges, labels=self.labels, include_lowest=True)
+            elif self.binning_method == 'kmeans':
+                for col in X.select_dtypes(include=['int', 'float']).columns:
+                    kmeans = KMeans(n_clusters=self.num_classes, random_state=0).fit(X[[col]])
+                    bin_edges = np.append(kmeans.cluster_centers_ - 0.5, kmeans.cluster_centers_[-1] + 0.5)
+                    self.bins[col] = pd.cut(X[col], bins=bin_edges, labels=self.labels, include_lowest=True)
             else:
                 self.bins[col] = pd.cut(X[col], self.num_classes, labels=self.labels)
 
@@ -69,7 +85,11 @@ class Bucketizer(BaseEstimator, TransformerMixin):
 
         transformed_data = X.copy()
         for col in self.bins:
-            transformed_data[col] = self.bins[col].astype(str)
+            if self.inplace:
+                transformed_data[col+'_bucketed'] = self.bins[col].astype(str)
+                transformed_data.drop(columns=col, inplace=True)
+            else:
+                transformed_data[col] = self.bins[col].astype(str)
 
         return transformed_data
 
@@ -99,4 +119,3 @@ class Bucketizer(BaseEstimator, TransformerMixin):
         """
         # Example of automatic num_classes calculation
         return int(np.sqrt(len(X)))
-
